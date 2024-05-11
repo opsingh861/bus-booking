49d5b3454fe3b582c5ec9026d0252bccfa3830ae
Sunny12110867 <sunnybhardwaj456789@gmail.com>
2024-05-11 13:09:23 +0530

    Changed the logo name

--- client/src/components/ui/Navbar.jsx
+++ client/src/components/ui/Navbar.jsx
@@ -6,7 +6,7 @@ function Navbar() {
     return (
         <nav className='bg-black text-white p-4'>
             <ul className='flex justify-around items-center cursor-pointer' >
-                <li className='italic bold text-yellow-500 text-2xl'><Link href='/'>GoRide</Link></li>
+                <li className='italic bold text-yellow-500 text-2xl'><Link href='/'>BusBuzz</Link></li>
                 <li><Link href='/about'>About Us</Link></li>
                 <li><Link href="/services">Packages</Link></li>
                 <li><Link href="/contact">Contact Us</Link></li>
